e26592918888da21ed5e25faf5cebd3c799564e6
Filipe Fasolin <ffasolin@gmail.com>
2017-05-10 16:33:07 +0100

    Adds weather feature

--- jasmine/spec/AirportSpec.js
+++ jasmine/spec/AirportSpec.js
@@ -1,7 +1,7 @@
 describe('Airport', function() {
 
-var airport;
-var plane;
+  var airport;
+  var plane;
 
   beforeEach(function() {
     airport = new Airport();
@@ -9,10 +9,34 @@ var plane;
 
   });
 
+  it('contains a plane after landing', function() {
+    airport.isStormy = function() { return false };
+    airport.land(plane);
+    expect(airport.runway).toContain(plane);
+  });
 
-it('contains a plane after landing', function() {
-  airport.land(plane);
-  expect(airport.runway).toContain(plane);
-});
+  it('takes off planes', function() {
+    airport.isStormy = function() { return false };
+    airport.land(plane);
+    airport.takeOff();
+    expect(airport.runway).toEqual([]);
+  });
 
+  it('prevents takeoff in stormy weather', function() {
+    airport.isStormy = function() { return false };
+    airport.land(plane);
+    airport.isStormy = function() { return true };
+    expect( function() {airport.takeOff(); } ).toThrow("Weather is stormy");
+  });
+
+  it('does not prevent takeoff in unstormy weather', function() {
+    airport.isStormy = function() { return false };
+    airport.land(plane);
+    expect( function() {airport.takeOff(); } ).not.toThrow("Weather is stormy");
+  });
+
+  it('prevents landing in stormy weather', function() {
+    airport.isStormy = function() { return true };
+    expect( function() { airport.land(plane); }).toThrow("Weather is stormy");
+  });
 });
--- jasmine/src/Airport.js
+++ jasmine/src/Airport.js
@@ -3,5 +3,25 @@ function Airport() {
 };
 
 Airport.prototype.land = function(plane) {
+  if (this.isStormy()) {
+    throw "Weather is stormy";
+  } else {
   this.runway.push(plane);
+  }
+};
+
+Airport.prototype.takeOff = function() {
+  if (this.isStormy()) {
+    throw "Weather is stormy";
+  } else {
+  this.runway.pop();
+  }
+};
+
+Airport.prototype.isStormy = function() {
+  if (Math.random() < 0.2) {
+    return true
+  } else {
+    return false
+  };
 };
